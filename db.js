3e133477e5f4619d021bfe20eacab9c24bdc12f9
mmirshad194-afk <mmirshad194@gmail.com>
2025-12-19 09:57:58 +0530

    change

---
+++ db.js
@@ -0,0 +1,21 @@
+const mysql=require('mysql');
+const express=require('express');
+const app=express();
+
+const connection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'payroll',
+});
+connection.connect((error)=>{
+    if(error){
+        console.log("error database connection");
+        return;
+    }
+    console.log("database connected");
+
+})
+app.get('/display',(req,res)=>{
+
+})
