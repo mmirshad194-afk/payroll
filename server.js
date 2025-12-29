8ee3ae3377f828cb49957774fc591e8338827197
mohammedsinan43340 <mohammedsinan43340@gmail.com>
2025-12-29 10:27:25 +0530

    Local changes in server.js

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ const employees=require('./employees');
 app.use('/emp',employees);
 const users=require('./payrolluser');
 app.use('/users',users);
+const attendance=require('./attendance');
+app.use('/att',attendance);
 
 
 app.listen(8000, () => console.log("Server running"));
